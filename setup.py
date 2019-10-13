d036d1033b4b1c3d15df1d051296415c80c3653b
Rehmat Alam <contact@rehmat.works>
2019-10-14 00:32:06 +0500

    Initlal project setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='spsuite',
+	version='1.0.0',
+	description='Command line utilities to manage ServerPilot provisioned servers.',
+	author="Rehmat Alam",
+	author_email="contact@rehmat.works",
+	url="https://github.com/rehmatworks/spsuite",
+	license="MIT",
+	entry_points={
+		'console_scripts': [
+			'spsuite = spsuite.spsuite:main'
+		],
+	},
+	packages=[
+		'spsuite'
+	],
+	install_requires=[
+		'python-nginx',
+		'validators'
+	]
+)

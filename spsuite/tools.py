714395160814a09a02892279ed9d6780b605d8e4
Rehmat Alam <contact@rehmat.works>
2019-10-15 11:29:51 +0500

    Importing required lib

--- spsuite/tools.py
+++ spsuite/tools.py
@@ -9,6 +9,7 @@ import pymysql
 import configparser
 import warnings
 import validators
+from getpass import getpass
 
 def du(path):
     return subprocess.check_output(['du','-sh', path]).split()[0].decode('utf-8')

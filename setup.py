8952371995c0f7b361f48674b3cff66edab3f281
Rehmat Alam <contact@rehmat.works>
2019-10-28 01:17:58 +0500

    Bumping to v1.0.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class SetupSslRenewCron(install):
 		install.run(self)
 
 setup(name='spsuite',
-	version='1.0.1',
+	version='1.0.2',
 	description='Command line utilities to manage ServerPilot provisioned servers.',
 	author="Rehmat Alam",
 	author_email="contact@rehmat.works",
